9464ac95f4d8eab2c552296bc9d7f1e93c9aa06d
tayjay1019 <tjoseph1@my.wctc.edu>
2021-08-31 13:20:55 -0500

    randomized the attention seeker animations

--- js/balloons.js
+++ js/balloons.js
@@ -16,3 +16,22 @@ $('.form-check-input').on('change', function () {
      $('#' + this.id + 'Img').removeClass().addClass('animate__animated animate__bounceInDown') :
      $('#' + this.id + 'Img').addClass('animate__animated animate__bounceOutUp');
 });
+
+let animations = ['animate__animated animate__bounch',
+                    'animate__animated animate__flash',
+                    'animate__animated animate__pulse',
+                    'animate__animated animate__rubberBand',
+                    'animate__animated animate__shakeX',
+                    'animate__animated animate__shakeY',
+                    'animate__animated animate__headShake',
+                    'animate__animated animate__swing',
+                    'animate__animated animate__tada',
+                    'animate__animated animate__wobble',
+                    'animate__animated animate__jello',
+                    'animate__animated animate__heartBeat'];
+
+var ran = Math.floor(Math.random() * animations.length);
+
+$(function(){
+    $('#birthdayTag').addClass(animations[ran]);
+});
